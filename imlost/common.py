9735e28c57db1fc0c5b363b5fb415279529cca4a
Xiayang Wu <youngng@gmail.com>
2013-06-09 22:55:41 -0700

    move common static vars to common.py

---
+++ imlost/common.py
@@ -0,0 +1,4 @@
+# static common stuffs
+USER_FIELDS = ['user_id', 'user_name', 'user_type', 'device_type', 'device_code', 'phone', 'password']
+SALT = "imLost5aBadsf,>213" # need to move to config
+ONLINE_THRESHOLD = 900 # 15 mins
