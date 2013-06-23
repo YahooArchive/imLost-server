97a79738d697347e229e0f8cc0a6ddb2c53f955e
Xiayang Wu <youngng@gmail.com>
2013-06-23 13:00:40 -0700

    bugfix

--- imlost/views.py
+++ imlost/views.py
@@ -66,7 +66,7 @@ def signout(request):
 def get_profile(request):
     user = get_current_user(request)
     for k in user.keys():
-        if k not in ['user_id','user_name','user_type','device_type','device_token','phone']:
+        if k not in ['user_id','user_name','user_type','device_type','device_token','phone', 'location']:
             user.pop(k)
     return response_wrapper(200, 'OK', {'user':user})
 
@@ -102,11 +102,13 @@ def get_contacts(request):
         contacts = []
     else:
         for c in contacts:
-            c.pop('_id')
-            if time.time() - c['last_seen'] < ONLINE_THRESHOLD:
+            if 'last_seen' in c and time.time() - c['last_seen'] < ONLINE_THRESHOLD:
                 c['online'] = True
             else:
                 c['online'] = False
+            for k in ['device_token', 'device_type']:
+                if k in c:
+                    c.pop(k)
     
     return response_wrapper(200, 'OK', {'contacts':contacts})
 
@@ -115,6 +117,8 @@ def add_contact(request):
     # send a request to another user to make a connection
     user = get_current_user(request)
     contact_id = request.params['contact_id'].lower()
+    if contact_id == user['user_id']:
+        return response_wrapper(403, 'You cannot add yourself as contact')
     # check if already in the contact list
     for c in user['contacts']:
         if c['user_id'] == contact_id:
@@ -133,14 +137,19 @@ def add_contact(request):
 def accept_contact(request):
     user = get_current_user(request)
     contact_id = request.params['contact_id'].lower()
-    if contact_id not in [c['user_id'] for c in user['contacts']]:
-        return response_wrapper(400, 'No such contact')
+    pending_contact = None
+    for c in user['contacts']:
+        if c['user_id'] == contact_id and c['status'] == 'pending':
+            pending_contact = c
+
+    if pending_contact is None:
+        return response_wrapper(400, 'No such new contact')
+
     userdb = request.db['users']
     userdb.update({'_id':user['_id'], 'contacts.user_id':contact_id}, 
                   {'$set':{'contacts.$.status':'accepted'}})
     user_data = extract_public_info(user)
     user_data['status'] = 'accepted'
-    print user_data
     userdb.update({'user_id':contact_id, 'contacts.user_id':user['user_id']}, 
                   {'$set':{'contacts.$':user_data}})
     return response_wrapper(200, 'OK')
@@ -150,12 +159,19 @@ def accept_contact(request):
 #     #TODO
 #     pass 
 
-@view_config(route_name='update_location', request_method='PUT')
+@view_config(route_name='location', request_method='PUT')
 def update_location(request):
     user = get_current_user(request)
     userdb = request.db['users']
-    lng = request.params['lng']
-    lat = request.params['lat']
+    try:
+        lng = float(request.params['lng'])
+        lat = float(request.params['lat'])
+    except:
+        return response_wrapper(500, 'Illegal lat lng value')
+
+    if not (abs(lng) <= 180 and abs(lat) <= 90):
+        return response_wrapper(500, 'Illegal lat lng value')
+
     userdb.update({'_id':user['_id']}, {'$set':{'last_seen':time.time(),'location':{'lng':lng,'lat':lat}}})
     userdb.update({'contacts.user_id':user['user_id']}, 
                   {'$set':{'contacts.$.last_seen':time.time(),'contacts.$.location':{'lng':lng,'lat':lat}}})
